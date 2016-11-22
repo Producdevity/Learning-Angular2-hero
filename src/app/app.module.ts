763f9c0a04d38019bc53f819499695c737be2383
Yassine Gherbi <yassine@DEV420.home>
2016-11-22 20:34:05 +0100

    first init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
+import './rxjs-extensions';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule }     from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero/hero-detail.component';
+import { HeroesComponent } from './hero/heroes.component';
+import { HeroService } from './hero/hero.service';
+import { HeroSearchComponent } from './hero/hero-search.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		DashboardComponent,
+		HeroDetailComponent,
+		HeroesComponent,
+		HeroSearchComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpModule,
+		InMemoryWebApiModule.forRoot(InMemoryDataService),
+		AppRoutingModule
+	],
+	providers: [HeroService],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
